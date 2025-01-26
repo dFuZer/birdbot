d93233dab785ec042e0e0730781b60fc8291926e
Enzo Tomassi <enzotomassi@gmail.com>
2025-01-26 22:12:35 +0100

    Add in-game bot with basic functionality
    Using a home-made socket.io client, the bot can now connect to a JKLM room and answer prompts.
    Features:
    - Connect to JKLM room
    - Listen for prompts and answer with a word using a 20-word dictionary
    - Auto-join every game

---
+++ bot/src/handle_message.rs
@@ -0,0 +1,39 @@
+mod parse_message;
+mod respond_message;
+
+use std::sync::Arc;
+
+use futures_util::stream::SplitSink;
+use respond_message::{respond_message_game, respond_message_room};
+use tokio::net::TcpStream;
+use tokio::sync::Mutex;
+use tokio_tungstenite::{MaybeTlsStream, WebSocketStream};
+use tungstenite::protocol::Message;
+
+use parse_message::parse_message;
+
+pub async fn handle_message_room(
+    msg: Message,
+    socket_write: &mut SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>,
+    room_state: &Arc<Mutex<super::RoomState>>,
+) {
+    let parsed_message = parse_message(msg).expect("Failed to read message");
+    println!(
+        "[room] [{}]: {}",
+        parsed_message.event_type, parsed_message.json
+    );
+    respond_message_room(parsed_message, socket_write, room_state).await;
+}
+
+pub async fn handle_message_game(
+    msg: Message,
+    socket_write: &mut SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>,
+    room_state: &std::sync::Arc<tokio::sync::Mutex<super::RoomState>>,
+) {
+    let parsed_message = parse_message(msg).expect("Failed to read message");
+    println!(
+        "[game] [{}]: {}",
+        parsed_message.event_type, parsed_message.json
+    );
+    respond_message_game(parsed_message, socket_write, room_state).await;
+}
