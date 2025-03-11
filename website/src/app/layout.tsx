f05d98dc9b98663b8bdcd7bb1c038796106a6df0
Enzo Tomassi <enzotomassi@gmail.com>
2025-03-11 08:17:45 +0100

    wip

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -6,6 +6,25 @@ export const metadata: Metadata = {
     description: "BirdBot is a JKLM.fun BombParty bot.",
 };
 
+function Blobs() {
+    return (
+        <div className="absolute top-0 right-0 left-0 -z-10 h-full overflow-hidden">
+            <div className="mt-40 flex w-full justify-between px-[6rem]">
+                <div className="bg-primary-500/80 mt-24 h-32 w-[20rem] blur-[180px]"></div>
+                <div className="bg-highlight-500/80 h-32 w-[20rem] blur-[180px]"></div>
+            </div>
+            <div className="mt-[32rem] flex w-full justify-between px-[6rem]">
+                <div className="bg-highlight-500/80 h-32 w-[20rem] blur-[180px]"></div>
+                <div className="bg-primary-500/80 mt-32 h-32 w-[20rem] blur-[180px]"></div>
+            </div>
+            <div className="mt-[35rem] flex w-full justify-between px-[6rem]">
+                <div className="bg-primary-500/80 mt-32 h-32 w-[20rem] blur-[180px]"></div>
+                <div className="bg-highlight-500/80 h-32 w-[20rem] blur-[180px]"></div>
+            </div>
+        </div>
+    );
+}
+
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -13,7 +32,11 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body className="relative min-h-screen">
+                <header className="z-50 h-[4.2rem] border-b border-neutral-200 bg-white/20 backdrop-blur-xs">birdbfffffot</header>
+                <Blobs />
+                <div>{children}</div>
+            </body>
         </html>
     );
 }
