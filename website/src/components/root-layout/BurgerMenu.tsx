b7e8dc800f852b173d08a064c975f2778300355d
Enzo Tomassi <enzo@miloctav.fr>
2025-03-19 13:31:42 +0100

    feat(website): make burger menu more accessible

--- website/src/components/root-layout/BurgerMenu.tsx
+++ website/src/components/root-layout/BurgerMenu.tsx
@@ -4,11 +4,12 @@ import { LINKS } from "@/lib/constants";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { createRef, useEffect, useState } from "react";
 
 export default function BurgerMenu() {
     let [open, setOpen] = useState(false);
     const location = usePathname();
+    const firstLinkRef = createRef<HTMLAnchorElement>();
 
     function close() {
         if (open) setOpen(false);
@@ -28,6 +29,12 @@ export default function BurgerMenu() {
         if (open) close();
     }, [location]);
 
+    useEffect(() => {
+        if (open) {
+            firstLinkRef.current?.focus();
+        }
+    }, [open]);
+
     return (
         <>
             <button onClick={invertOpen} className="flex h-8 w-8 items-center justify-center">
@@ -44,9 +51,12 @@ export default function BurgerMenu() {
                 className={`absolute top-[3.5rem] left-0 w-full overflow-hidden bg-neutral-50 transition-all duration-350 ${open ? "h-[calc(100vh-3.5rem)]" : "h-0"}`}
             >
                 <div className="space-y-1 py-2">
-                    {LINKS.map((link) => (
+                    {LINKS.map((link, index) => (
                         <Link
+                            ref={index === 0 ? firstLinkRef : undefined}
                             key={link.href}
+                            tabIndex={open ? 0 : -1}
+                            role="menuitem"
                             onClick={() => closeIfSameLocation(link.href)}
                             className="mx-2 flex items-center rounded-full px-8 py-3 font-semibold hover:bg-neutral-200"
                             href={link.href}
