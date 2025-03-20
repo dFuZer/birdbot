7ae49a14a751a0399ae7815d91df49ba97acc44c
Enzo Tomassi <enzotomassi@gmail.com>
2025-03-21 00:35:32 +0100

    wip

--- website/src/app/records/page.tsx
+++ website/src/app/records/page.tsx
@@ -1,5 +1,6 @@
 import RecordsPage from "@/components/pages/RecordsPage/RecordsPage";
+import { TSearchParams } from "@/types";
 
-export default function Page() {
+export default function Page({ searchParams }: { searchParams: TSearchParams }) {
     return <RecordsPage />;
 }
