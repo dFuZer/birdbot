0a911217a56317d9d8e3396cbfd6c96e69b84fa7
Enzo Tomassi <enzotomassi@gmail.com>
2025-03-24 13:02:08 +0100

    fix(website): fixed svg webpack config

--- website/next.config.ts
+++ website/next.config.ts
@@ -21,11 +21,25 @@ const nextConfig: NextConfig = {
         },
     },
     webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/i,
-            issuer: /\.[jt]sx?$/,
-            use: ["@svgr/webpack"],
-        });
+        // SVGR config
+        /* eslint-disable @typescript-eslint/no-explicit-any */
+        const fileLoaderRule = config.module.rules.find((rule: any) => rule.test?.test?.(".svg"));
+
+        config.module.rules.push(
+            {
+                ...fileLoaderRule,
+                test: /\.svg$/i,
+                resourceQuery: /url/,
+            },
+            {
+                test: /\.svg$/i,
+                issuer: fileLoaderRule.issuer,
+                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+                use: ["@svgr/webpack"],
+            }
+        );
+
+        fileLoaderRule.exclude = /\.svg$/i;
 
         return config;
     },
