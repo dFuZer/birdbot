8b88b1e0a457cb90094fa263aa14fd0a3eec358f
Enzo Tomassi <enzotomassi@gmail.com>
2025-03-26 10:23:15 +0100

    refactor/feat(api): added game recaps
    added entity in database schema, route, and zod schema to create game recaps
    improved ordering of fields in prisma schema

---
+++ api/routes/addGameRecap.route.ts
@@ -0,0 +1,48 @@
+import { type RouteHandlerMethod } from "fastify";
+import addGameIfNotExist from "../helpers/addGameIfNotExist";
+import addPlayerIfNotExist from "../helpers/addPlayerIfNotExist";
+import Logger from "../lib/logger";
+import prisma from "../prisma/client";
+import { gameRecap } from "../schemas/game.zod";
+
+export let addWordRouteHandler: RouteHandlerMethod = async function (req, res) {
+    let requestJson = req.body;
+    let parsed = gameRecap.safeParse(requestJson);
+    if (!parsed.success) {
+        Logger.error({ message: "Input rejected by Zod", path: "addGameRecap.route.ts", errorType: "zod", error: parsed.error });
+        return res.status(400).send({ message: "Invalid input!" });
+    }
+
+    try {
+        const gameRecapData = parsed.data;
+        const [game, player] = await Promise.all([addGameIfNotExist(gameRecapData.game), addPlayerIfNotExist(gameRecapData.player)]);
+        Logger.log({ message: `Inserting game recap: ${game.id}, ${player.id}`, path: "addGameRecap.route.ts" });
+        await prisma.$executeRaw`
+            INSERT INTO game_recap (game_id,
+            player_id,
+            words_count,
+            flips_count,
+            depleted_syllables_count,
+            alpha_count,
+            words_without_death_count,
+            previous_syllables_count,
+            multi_syllables_count,
+            hyphen_words_count,
+            more_than_20_letters_words_count)
+            VALUES (${game.id}::UUID,
+            ${player.id}::UUID,
+            ${gameRecapData.wordsCount},
+            ${gameRecapData.flipsCount},
+            ${gameRecapData.depletedSyllablesCount},
+            ${gameRecapData.alphaCount},
+            ${gameRecapData.wordsWithoutDeathCount},
+            ${gameRecapData.previousSyllablesCount},
+            ${gameRecapData.multiSyllablesCount},
+            ${gameRecapData.hyphenWordsCount},
+            ${gameRecapData.moreThan20LettersWordsCount})
+        `;
+    } catch (e) {
+        Logger.error({ message: "Failed to add game recap", path: "addGameRecap.route.ts", errorType: "unknown", error: e });
+        return res.status(500).send({ message: "Failed to add game recap" });
+    }
+};
--- api/schemas/game.zod.ts
+++ api/schemas/game.zod.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { playerSchema } from "./player.zod";
 
 let languages = z.enum(["FR", "EN", "DE", "ES", "BRPT"]);
 let modes = z.enum(["REGULAR", "EASY", "BLITZ", "SUB500", "SUB50", "FREEPLAY"]);
@@ -9,4 +10,27 @@ let gameSchema = z.object({
     mode: modes,
 });
 
-export { gameSchema };
+let gameRecap = z.object({
+    game: gameSchema,
+    player: playerSchema,
+
+    // Non-listed records
+    startTimestamp: z.number(),
+    endTimestamp: z.number(),
+    wordsCount: z.number(),
+    flipsCount: z.number(),
+    depletedSyllablesCount: z.number(),
+    alphaCount: z.number(),
+    wordsWithoutDeathCount: z.number(),
+    previousSyllablesCount: z.number(),
+    multiSyllablesCount: z.number(),
+
+    // Semi-listed records
+    hyphenWordsCount: z.number(),
+    moreThan20LettersWordsCount: z.number(),
+
+    // Listed records
+    // (None)
+});
+
+export { gameRecap, gameSchema };
