015c0ef5f9443c76badf4b67ef4b9a425dcd6dd7
Enzo Tomassi <enzotomassi@gmail.com>
2025-03-26 10:35:36 +0100

    feat(api): added game recaps route listener

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import Fastify from "fastify";
 import Logger from "./lib/logger";
 import { authMiddleware } from "./middleware/auth";
+import { addGameRecapRouteHandler } from "./routes/addGameRecap.route";
 import { addPlayersRouteHandler } from "./routes/addPlayers.route";
 import { addWordRouteHandler } from "./routes/addWord.route";
 
@@ -13,6 +14,7 @@ app.addHook("preHandler", authMiddleware);
 // API Routes
 app.post("/add-word", addWordRouteHandler);
 app.post("/add-player", addPlayersRouteHandler);
+app.post("/add-game-recap", addGameRecapRouteHandler);
 
 // Start the API
 app.listen({ port: PORT }, (err) => {
