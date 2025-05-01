ac745ab4af4db14f90d3ca404b9b056bb327f1da
Enzo Tomassi <enzotomassi@gmail.com>
2025-05-01 14:58:37 +0200

    chore: edit discord invitation link

--- website/src/lib/links.ts
+++ website/src/lib/links.ts
@@ -1,4 +1,4 @@
-export const DISCORD_SERVER_LINK = "https://discord.gg/SqGEM64bCF";
+export const DISCORD_SERVER_LINK = "https://discord.com/invite/J3yHjv4ZUd";
 export const GITHUB_REPO_LINK = "https://github.com/dFuZer/birdbot";
 export const PAYPAL_DONATE_LINK = "https://www.paypal.com/paypalme/enzotomassi";
 
