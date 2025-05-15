fd70125f6ab652ea64c68b83e0ef076c68dcd445
Enzo Tomassi <enzotomassi@gmail.com>
2025-05-15 18:17:19 +0200

    fix: link to bot website

--- bot/src/bots/birdbot/BirdBotConstants.ts
+++ bot/src/bots/birdbot/BirdBotConstants.ts
@@ -14,7 +14,7 @@ import {
 export const DISCORD_SERVER_LINK = "https://discord.com/invite/J3yHjv4ZUd";
 export const GITHUB_REPO_LINK = "https://github.com/dFuZer/birdbot";
 export const PAYPAL_DONATE_LINK = "https://www.paypal.com/paypalme/enzotomassi";
-export const WEBSITE_LINK = "https://birdbot.gg";
+export const WEBSITE_LINK = "https://birdbot.dev";
 
 const birdbotRulesBase = {
     roundsToWin: 1,
