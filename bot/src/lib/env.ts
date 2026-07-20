9d2d0bf4b8195ea9e1f32a4359b24350a55eab43
Enzo Tomassi <enzotomassi@gmail.com>
2026-07-20 19:13:39 +0200

    port profile picture

--- bot/src/lib/env.ts
+++ bot/src/lib/env.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import { DEFAULT_BOT_PICTURE } from "./constants/defaultBotPicture";
 dotenv.config();
 
 const NAMESPACE_UUID = process.env.NAMESPACE_UUID as string;
@@ -9,7 +10,7 @@ const JKLM_AUTH_EXPIRATION = process.env.JKLM_AUTH_EXPIRATION
     ? Number(process.env.JKLM_AUTH_EXPIRATION)
     : 2982251755416;
 const JKLM_LANGUAGE = (process.env.JKLM_LANGUAGE as string) || "en-US";
-const JKLM_PICTURE = (process.env.JKLM_PICTURE as string) || null;
+const JKLM_PICTURE = (process.env.JKLM_PICTURE as string) || DEFAULT_BOT_PICTURE;
 
 if (!NAMESPACE_UUID) {
     throw new Error("Missing NAMESPACE_UUID environment variable");
